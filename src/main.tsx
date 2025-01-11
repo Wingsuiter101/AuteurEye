bbf3e9e0082f98f8a04fab1973bdd6529b41682e
Wingsuiter101 <sauravdh101@gmail.com>
2025-01-11 16:18:49 -0500

    small changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Handle redirect from 404 page
+const redirectHandler = () => {
+  // Get the redirect path from the URL
+  const pathname = window.location.search.match(/^\?(.+)$/)?.[1];
+  if (pathname) {
+    // Replace the URL with the actual path
+    window.history.replaceState(null, '', '/AuteurEye/' + pathname);
+  }
+};
+
+redirectHandler();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
