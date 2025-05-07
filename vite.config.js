bb6a823695dd70817b944bb3492ffb20a9c85ee8
SauravDH <dhun0009@algonquinlive.com>
2025-05-07 03:47:54 -0400

    Update build files and configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/AuteurEye/', // Add this line for GitHub Pages
     plugins: [react()],
     resolve: {
         alias: {
